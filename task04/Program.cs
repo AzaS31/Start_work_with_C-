7c3287dda2e0d51a70c91ed258570f469bbd048b
Azamat <azamatsalaly@gmail.com>
2023-12-21 20:00:30 +0600

    added task04

---
+++ task04/Program.cs
@@ -0,0 +1,33 @@
+﻿int a = 5;
+int b = 3;
+int c = 7;
+int d = 9;
+int e = 2;
+
+int max = 0;
+
+if (max < a)
+{
+    max = a;
+}
+
+if (max < b)
+{
+    max = b;
+}
+
+if (max < c)
+{
+    max = c;
+}
+
+if (max < d)
+{
+    max = d;
+}
+
+if (max < e)
+{
+    max = e;
+}
+Console.WriteLine(max);
