f17179799b5c3be005bf8f7670fd114898d87f63
Azamat <azamatsalaly@gmail.com>
2023-12-21 19:03:07 +0600

    added some variables lesson01:task01

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+
+
+int number = 100;
+Console.WriteLine(number);
+
+double d =5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
